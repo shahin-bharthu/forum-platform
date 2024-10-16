d2a52261c93e77242f2113b889c50e043a06f9a3
isha23pix <ishasamal23@gmail.com>
2024-10-16 14:35:39 +0530

    Separated the routes from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,8 @@
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import { createBrowserRouter,RouterProvider, Navigate } from "react-router-dom";
 
 import "./App.css";
-import SignupPage from "./pages/Signup";
-import LoginPage from "./pages/Login";
-import AuthPage from "./pages/AuthPage";
-import UserProfilePage from "./pages/UserProfilePage";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
-
+import router from './routes.jsx'
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <AuthPage />,
-      children: [
-        {index: true, element: <Navigate to="/signup" replace /> },
-        {path:'signup' , element:<SignupPage /> },
-        {path:'login' , element:<LoginPage/>  },
-        {path:'user-profile', element: <UserProfilePage/> },
-        {path:'forgot-password', element: <ForgotPassword/> },
-        {path:'reset-password/:token', element: <ResetPassword/> }
-      ],
-    },
-  ]);
   return (
     <>
     <RouterProvider router={router}></RouterProvider>
