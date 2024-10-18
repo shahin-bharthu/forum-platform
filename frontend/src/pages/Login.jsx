96ad3efb131cbca3007a11cbdc866ad355730240
shahin-bharthu <shahinbharthu@gmail.com>
2024-10-18 17:22:57 +0530

    fixed login/signup routes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,11 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { Navigate, useRouteLoaderData } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
-import Dashboard from '../pages/Dashboard/Index';
 
 const LoginPage = () => {
     const token = useRouteLoaderData('root');
     return (
         <>
-            {token && <Dashboard />}
+            {token && <Navigate to="/dashboard" />}
             {!token && <LoginForm/>}
         </>
     )
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,13 +1,13 @@
+import { Navigate } from "react-router-dom";
 import { getAuthToken } from "../../utils/auth";
 import SignupForm from "../components/SignupForm";
-import Dashboard  from "../pages/Dashboard/Index.jsx";
 
 const SignupPage = () => {
     const token = getAuthToken();
 
     return (
         <>
-        {token && <Dashboard />}
+        {token && <Navigate to="/dashboard" />}
         {!token && <SignupForm/>}
         </>
     )
