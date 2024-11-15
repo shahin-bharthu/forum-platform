0bad7191b7adba5883e24f780b96c1e126c8a427
shahin-bharthu <shahinbharthu@gmail.com>
2024-11-15 19:59:50 +0530

    fix: create post

--- frontend/src/pages/CreatePost/Components/SelectList.jsx
+++ frontend/src/pages/CreatePost/Components/SelectList.jsx
@@ -1,143 +1,121 @@
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import CircularProgress from '@mui/material/CircularProgress';
-import { useState } from 'react';
-import useForumNames from '../Hooks/useForumNames';
+// import TextField from "@mui/material/TextField";
+// import Autocomplete from "@mui/material/Autocomplete";
+// import CircularProgress from "@mui/material/CircularProgress";
+// import { useState } from "react";
+// import useForumNames from "../Hooks/useForumNames";
 
-export default function Asynchronous() {
-    const { forums, isLoading, error } = useForumNames();
-    const [open, setOpen] = useState(false);
-    const [options, setOptions] = useState([]);
+// export default function Asynchronous({ getForum }) {
+//   const { forums, isLoading, error } = useForumNames();
+//   const [open, setOpen] = useState(false);
+//   const [options, setOptions] = useState([]);
+//   const [selectedForum, setSelectedForum] = useState(null);
 
-    const handleOpen = () => {
-        setOpen(true);
-        setOptions([...forums]);
-    };
+//   const handleOpen = () => {
+//     setOpen(true);
+//     setOptions([...forums]);
+//   };
 
-    const handleClose = () => {
-        setOpen(false);
-        setOptions([]);
-    };
+//   const handleClose = () => {
+//     setOpen(false);
+//     setOptions([]);
+//   };
 
-    return (
-        <Autocomplete
-            sx={{
-                mt:2, width: '40%', 
-                '& .MuiOutlinedInput-root': {
-                    borderRadius: '28px',
-                },
-                '& .MuiAutocomplete-paper': {
-                    borderRadius: '15px',
-                }
-            }}
-            open={open}
-            onOpen={handleOpen}
-            onClose={handleClose}
-            isOptionEqualToValue={(option, value) => option.name === value.name}
-            getOptionLabel={(option) => option.name}
-            options={options}
-            loading={isLoading}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label="Forum"
-                    placeholder='Select a Forum'
-                    slotProps={{
-                        input: {
-                            ...params.InputProps,
-                            endAdornment: (
-                                <>
-                                    {isLoading ? <CircularProgress color="inherit" size={20} /> : null}
-                                    {params.InputProps.endAdornment}
-                                </>
-                            ),
-                        },
-                    }}
-                />
-            )}
-        />
-    );
+//   const triggerCallback = (event) => {
+//     event.preventDefault();
+//     // setSelectedForum(event.target.value)
+//     // if (selectedForum) {
+//       getForum(event.target.value);
+//     // }
+//     console.log(event.target.value);
+//   };
+
+//   return (
+//     <form>
+//       <Autocomplete
+//         sx={{
+//           mt: 2,
+//           width: "40%",
+//           "& .MuiOutlinedInput-root": {
+//             borderRadius: "28px",
+//           },
+//           "& .MuiAutocomplete-paper": {
+//             borderRadius: "15px",
+//           },
+//         }}
+//         open={open}
+//         onOpen={handleOpen}
+//         onClose={handleClose}
+//         isOptionEqualToValue={(option, value) => option.name === value.name}
+//         getOptionLabel={(option) => option.name}
+//         options={options}
+//         loading={isLoading}
+//         // onChange={triggerCallback}
+//         renderInput={(params) => (
+//           <TextField
+//             {...params}
+//             label="Forum"
+//             onChange={triggerCallback}
+//             placeholder="Select a Forum"
+//             slotProps={{
+//               input: {
+//                 ...params.InputProps,
+//                 endAdornment: (
+//                   <>
+//                     {isLoading ? (
+//                       <CircularProgress color="inherit" size={20} />
+//                     ) : null}
+//                     {params.InputProps.endAdornment}
+//                   </>
+//                 ),
+//               },
+//             }}
+//           />
+//         )}
+//       />
+//     </form>
+//   );
+// }
+
+// onChange={(event, newValue) => {
+//     setSelectedForum(newValue);  // Update selectedForum state when a forum is selected
+// }}
+
+
+
+import {useState} from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import useForumNames from "../Hooks/useForumNames";
+
+export default function SelectList({getForum}) {
+  const { forums, error } = useForumNames();
+  const [forum, setForum] = useState('');
+  
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setForum(event.target.value);
+    getForum(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Forum</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={forum}
+          label="Select Forum"
+          onChange={handleChange}
+        >
+        {forums.map((val) => 
+          <MenuItem value={val.id}>{val.name}</MenuItem>
+        )}
+        </Select>
+      </FormControl>
+    </Box>
+  );
 }
-
-
-
-// import { useTheme } from '@mui/material/styles';
-// import OutlinedInput from '@mui/material/OutlinedInput';
-// import MenuItem from '@mui/material/MenuItem';
-// import FormControl from '@mui/material/FormControl';
-// import Select from '@mui/material/Select';
-// import { useCallback, useState } from 'react';
-
-// const ITEM_HEIGHT = 48;
-// const ITEM_PADDING_TOP = 8;
-// const MenuProps = {
-//     PaperProps: {
-//         style: {
-//             maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-//             width: 250,
-//         },
-//     },
-// };
-
-// // Then use it:
-// function getStyles(forum, selectedName, theme) {
-//     return {
-//         fontWeight: forum.includes(selectedName)
-//             ? theme.typography.fontWeightMedium
-//             : theme.typography.fontWeightRegular
-//     };
-// }
-
-// export default function MultipleSelectPlaceholder() {
-//     const theme = useTheme();
-//     const { forums, isLoading, error } = useForumNames();
-//     const [forumChoice, setForumChoice] = useState('');
-
-//     const handleChange = useCallback((event) => {
-//         setForumChoice(event.target.value);
-//     }, []);
-
-//     const renderValue= useCallback((selected) => {
-//         if (!selected) {
-//             return <em>Forum</em>;
-//         }
-//         return selected
-//     },[])
-
-//     if (isLoading) {
-//         return <div>Loading...</div>;
-//     }
-
-//     if (error) {
-//         return <div>Error: {error}</div>;
-//     }
-
-
-//     return (
-//         <div>
-//             <FormControl variant='filled' sx={{ m: 1,  width: { xs: '100%', sm: '50ch', md: '50ch', lg: '70ch' }, mt: 3 }}>
-//                 <Select
-//                     displayEmpty
-//                     value={forumChoice}
-//                     onChange={handleChange}
-//                     input={<OutlinedInput />}
-//                     renderValue={renderValue}
-//                     MenuProps={MenuProps}
-//                 >
-//                     <MenuItem disabled value="">
-//                         <em>Forum</em>
-//                     </MenuItem>
-//                     {forums.map((forum) => (
-//                         <MenuItem
-//                             key={forum.id}
-//                             value={forum.name}
-//                             style={getStyles(forum.name, forumChoice, theme)}
-//                         >
-//                             {forum.name}
-//                         </MenuItem>
-//                     ))}
-//                 </Select>
-//             </FormControl>
-//         </div>
-//     );
-// }
--- frontend/src/pages/CreatePost/Index.jsx
+++ frontend/src/pages/CreatePost/Index.jsx
@@ -3,24 +3,24 @@ import CustomButton from "../../components/Button";
 import TextAreaInputField from "./Components/TextAreaInput.jsx";
 import TextInputField from "./Components/TextInput.jsx";
 import axios from "axios";
-import { z } from "zod";
 import Button from "@mui/material/Button";
 import { useNavigate, useRouteLoaderData } from "react-router-dom";
-import Asynchronous from "./Components/SelectList";
+import SelectList from "./Components/SelectList.jsx";
 import Stack from '@mui/material/Stack';
-import { Box, Card } from "@mui/material";
+import { Card } from "@mui/material";
 
 const CreatePost = () => {
   const token = useRouteLoaderData('root');
   const titleInput = useRef();
   const bodyInput = useRef();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const [errors, setErrors] = useState({});
+  const [selectedForum, setSelectedForum] = useState(null); 
 
   const handleInputChange = (event) => {
     const { name } = event.target;
@@ -34,29 +34,42 @@ const CreatePost = () => {
     setErrorMessage("");
   };
 
+  const getSelectedForumFromList = (selectedForumFromList) => {
+    console.log(selectedForumFromList);
+    setSelectedForum(selectedForumFromList); 
+  };
+
   async function submitHandler(event) {
     event.preventDefault();
 
     const enteredTitle = titleInput.current.value.trim();
     const enteredBody = bodyInput.current.value.trim();
 
-    const formData = { title: enteredTitle, body: enteredBody }
+    if (!selectedForum) {
+      setErrorMessage("Please select a forum.");
+      return;
+    }
 
-    // if (!validateForm(formData)) {
-    //   return;
-    // }
+    const formData = { 
+      title: enteredTitle, 
+      content: enteredBody, 
+      forum_id: selectedForum
+    };
+    console.log(formData);
+
     setErrorMessage("");
     setErrors({});
 
     try {
       setIsSubmitting(true);
-      const response = await axios.post("http://localhost:8080/forumgfd", formData, {
-        "Content-Type": "application/json",
+      const response = await axios.post("http://localhost:8080/topic", formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
         withCredentials: true
-      })
+      });
       setSuccessMessage(response.data.message);
-
-      //   setIsSubmitting(false);
+      navigate("/user/dashboard"); 
     } catch (error) {
       setIsSubmitting(false);
       console.error("Error:", error);
@@ -67,43 +80,48 @@ const CreatePost = () => {
   }
 
   return (
-    <Card variant="outlined" sx={{ m:2,p:3,justifyContent: 'left' }}>
+    <Card variant="outlined" sx={{ m: 2, p: 3, justifyContent: 'left' }}>
       <h3>Create Post</h3>
-      <Asynchronous />
-      <TextInputField
-        label="Title "
-        type="text"
-        name="title"
-        placeholder="Title"
-        reference={titleInput}
-        onChange={handleInputChange}
-        onFocus={handleInputFocus}
-      />
-      <TextAreaInputField
-        label="Body"
-        type="text"
-        name="Body"
-        placeholder="Body"
-        reference={bodyInput}
-        onChange={handleInputChange}
-        onFocus={handleInputFocus}
-      />
-      {/* <br /> */}
+      <form onSubmit={submitHandler}>
 
-      <Stack spacing={2} direction="row" sx={{m:1,pt:2, justifyContent:'right'}}>
-        <CustomButton
-          type="submit"
-          label={isSubmitting ? "Creating Post" : "Create Post"}
-          disabled={isSubmitting}
+        <SelectList getForum={getSelectedForumFromList}/>
+
+        <TextInputField
+          label="Title"
+          type="text"
+          name="title"
+          placeholder="Title"
+          reference={titleInput}
+          // onChange={handleInputChange}
+          // onFocus={handleInputFocus}
         />
-        <Button disableElevation onClick={() => { navigate('/user/dashboard') }}>
-          Back
-        </Button>
-      </Stack>
+
+        <TextAreaInputField
+          label="Body"
+          type="text"
+          name="Body"
+          placeholder="Body"
+          reference={bodyInput}
+          // onChange={handleInputChange}
+          // onFocus={handleInputFocus}
+        />
+
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+
+        <Stack spacing={2} direction="row" sx={{ m: 1, pt: 2, justifyContent: 'right' }}>
+          <CustomButton
+            type="submit"
+            label={isSubmitting ? "Creating Post..." : "Create Post"}
+            disabled={isSubmitting}
+          />
+          <Button disableElevation onClick={() => { navigate('/user/dashboard') }}>
+            Back
+          </Button>
+        </Stack>
+      </form>
     </Card>
   );
 };
 
 export default CreatePost;
-
-
