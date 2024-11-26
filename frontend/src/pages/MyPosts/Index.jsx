d1f897a323da284c78b04f353152d39e4bad1581
isha23pix <ishasamal23@gmail.com>
2024-11-26 15:19:58 +0530

    added edit and deoete post

--- frontend/src/pages/MyPosts/Index.jsx
+++ frontend/src/pages/MyPosts/Index.jsx
@@ -1,5 +1,5 @@
+
 import { useState, useEffect } from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
@@ -13,9 +13,12 @@ import axios from 'axios';
 import { Box } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import Avatar from '@mui/material/Avatar';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import PositionedSnackbar from '../../components/SnackBar';
-import { useNavigate } from 'react-router-dom';
+import { styled, alpha } from '@mui/material/styles';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
 
 const StyledCardHeader = styled(CardHeader)(({ theme }) => ({
   '.MuiCardHeader-content': {
@@ -29,6 +32,49 @@ const StyledCardHeader = styled(CardHeader)(({ theme }) => ({
   '.MuiCardHeader-subheader': {
     margin: 0,
   }
+}));
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color: 'rgb(55, 65, 81)',
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+    ...theme.applyStyles('dark', {
+      color: theme.palette.grey[300],
+    }),
+  },
 }));
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -58,32 +104,21 @@ export default function MyPosts() {
   const [forumTopics, setForumTopics] = useState([{ title: 'topic title', content: 'topic content', username: 'username' }]);
   const [expanded, setExpanded] = useState([{ isExpanded: false }]);
   const [forumBanner, setForumBanner] = useState({})
-  const [message, setMessage] = useState();
-  const navigate = useNavigate();
   //   const {topics} = useLoaderData();
-
-  const handleDeleteTopic = async (event, id) => {
-    const response = await axios.delete(`http://localhost:8080/topic/${id}`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true
-    });
-    setMessage('Post deleted');
-    setTimeout(() => {
-      setMessage(null);
-      window.location.reload();
-    }, 1000);
-  }
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
-    // console.log("useeffect");
-
     async function getForumTopics() {
       const myTopics = await axios.get(`http://localhost:8080/topic/my-topics`, {
         withCredentials: true,
       });
-      // console.log(myTopics);
 
       const myTopicsData = myTopics.data.data;
       const forumsList = await Promise.all(myTopicsData.map(forumTopic => axios.get(`http://localhost:8080/forum/${forumTopic.forum_id}`, { withCredentials: true })))
@@ -144,11 +179,9 @@ export default function MyPosts() {
     return (
       <>
         <Box mb={2}>
-
           <Typography variant="h5" component="div" sx={{ textAlign: "center", py: 5 }}>
             No Posts Yet!
           </Typography>
-
         </Box>
       </>
     )
@@ -157,10 +190,7 @@ export default function MyPosts() {
   return (
     <>
       <Grid size={12} sx={{ width: '100%', px: 3, mt: 10, alignSelf: 'start' }} >
-      {message && (
-        <PositionedSnackbar message={message} />
-      )}        
-      {forumTopics.map((topic, index) =>
+        {forumTopics.map((topic, index) =>
           <Box key={index} mb={2}>
             <Card >
               <StyledCardHeader
@@ -170,9 +200,37 @@ export default function MyPosts() {
                   </Avatar>
                 }
                 action={
-                  <IconButton aria-label="settings" onClick={(event) => handleDeleteTopic(event, topic.id)}>
-                    <DeleteOutlineIcon />
-                  </IconButton>
+                  <>
+                    <IconButton aria-label="settings"
+                      aria-controls={open ? 'demo-customized-menu' : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open ? 'true' : undefined}
+                      onClick={handleClick}>
+                      <MoreVertIcon />
+                    </IconButton>
+                    <StyledMenu
+                      id="demo-customized-menu"
+                      MenuListProps={{
+                        'aria-labelledby': 'demo-customized-button',
+                      }}
+                      anchorEl={anchorEl}
+                      open={open}
+                      onClose={handleClose}
+                    >
+                      <MenuItem onClick={handleClose} disableRipple>
+                        <EditIcon />
+                        Edit
+                      </MenuItem>
+                      <MenuItem onClick={handleClose} disableRipple>
+                        <FileCopyIcon />
+                        Delete
+                      </MenuItem>
+                      <MenuItem onClick={handleClose} disableRipple>
+                        <ArchiveIcon />
+                        Archive
+                      </MenuItem>
+                    </StyledMenu>
+                  </>
                 }
                 title={topic.forumname}
                 subheader={new Date(topic.createdAt).toLocaleDateString('en-US', {
@@ -182,7 +240,7 @@ export default function MyPosts() {
                 })}
               />
               <CardContent sx={{ py: 0, px: 3 }} >
-                <Typography variant="h6" sx={{ textAlign: 'left',wordBreak: 'break-word' }}>
+                <Typography variant="h6" sx={{ textAlign: 'left', wordBreak: 'break-word' }}>
                   {topic.title}
                 </Typography>
               </CardContent>
@@ -199,7 +257,7 @@ export default function MyPosts() {
               </CardActions>
               <Collapse in={expanded[index].isExpanded} timeout="auto" unmountOnExit>
                 <CardContent sx={{ px: 3 }}>
-                  <Typography variant='body2' sx={{ marginBottom: 2, textAlign: 'left', wordBreak:'break-word'}}>
+                  <Typography variant='body2' sx={{ marginBottom: 2, textAlign: 'left', wordBreak: 'break-word' }}>
                     {topic.content}
                   </Typography>
                 </CardContent>
